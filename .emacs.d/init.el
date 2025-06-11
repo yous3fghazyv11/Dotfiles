5092dac5ccaa323cd7442ef9408c01f28723a742
yous3fghazyv11 <yous3fghazyv11@gmail.com>
2025-06-11 20:28:03 +0300

    minor adjustments

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,3 +2,25 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(ef-dark))
+ '(custom-safe-themes
+   '("59c36051a521e3ea68dc530ded1c7be169cd19e8873b7994bfc02a216041bf3b"
+     "1ad12cda71588cc82e74f1cabeed99705c6a60d23ee1bb355c293ba9c000d4ac"
+     default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(highlight-indent-guides-character-face ((t (:foreground "dim gray"))))
+ '(internal-border ((t (:background "unspecified-bg"))))
+ '(org-level-1 ((t (:inherit outline-1 :height 2.0))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.8))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.6))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.4))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.2)))))
