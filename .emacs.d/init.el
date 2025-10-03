8b2c03e4f67e2cb00f5db19464f3ba100b53a868
yous3fghazyv11 <yous3fghazyv11@gmail.com>
2025-10-03 05:41:01 +0300

    switching to niri

---
+++ .emacs.d/init.el
@@ -0,0 +1,58 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(custom-enabled-themes '(gruvbox-dark-hard))
+;;  '(custom-safe-themes
+;;    '("d445c7b530713eac282ecdeea07a8fa59692c83045bf84dd112dd738c7bcad1d"
+;;      default)))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(highlight-indent-guides-character-face ((t (:foreground "dim gray"))))
+;;  '(internal-border ((t (:background "white"))))
+;;  '(org-block ((t (:background "#000000" :extend t))))
+;;  '(org-block-begin-line ((t (:background "#000000" :extend t))))
+;;  '(org-block-end-line ((t (:background "#000000" :extend t))))
+;;  '(org-level-1 ((t (:inherit outline-1 :height 2.0))))
+;;  '(org-level-2 ((t (:inherit outline-2 :height 1.8))))
+;;  '(org-level-3 ((t (:inherit outline-3 :height 1.6))))
+;;  '(org-level-4 ((t (:inherit outline-4 :height 1.4))))
+;;  '(org-level-5 ((t (:inherit outline-5 :height 1.2)))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(gruvbox-dark-hard))
+ '(custom-safe-themes
+   '("98ef36d4487bf5e816f89b1b1240d45755ec382c7029302f36ca6626faf44bbd"
+     "ba323a013c25b355eb9a0550541573d535831c557674c8d59b9ac6aa720c21d3"
+     "a5270d86fac30303c5910be7403467662d7601b821af2ff0c4eb181153ebfc0a"
+     "d445c7b530713eac282ecdeea07a8fa59692c83045bf84dd112dd738c7bcad1d"
+     default))
+ '(org-agenda-files
+   '("~/org/music.org" "/home/yousef/org/diary.org"
+     "/home/yousef/org/my_todo_list.org" "/home/yousef/org/study.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(internal-border ((t (:background "white"))))
+ '(org-block ((t (:background "#000000" :extend t))))
+ '(org-block-begin-line ((t (:background "#000000" :extend t))))
+ '(org-block-end-line ((t (:background "#000000" :extend t))))
+ '(org-level-1 ((t (:inherit outline-1 :height 2.0))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.8))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.6))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.4))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.2))))
+ '(org-modern-block ((t (:background "#1e1e1e" :foreground "#bbbbbb" :inherit fixed-pitch)))))
